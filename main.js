674f0fc6a1d24894ac7404729352e3c4fe0a6422
tomateco <gofer_joule0y@icloud.com>
2023-10-15 13:02:19 +0200

    api fetch and some basic funtionality

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+let movieNameRef = document.getElementById('movieName');
+let btnRef = document.getElementById('searchBtn');
+let result = document.getElementById('result');
 
+//fetch data fn
+async function fetchMovie() {
+  let title = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${title}&apikey=${key}`;
+  if (title.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please enter movie title</h3>`;
+  } else {
+    const response = await fetch(url);
+    const movie = await response.json();
+    console.log(movie);
+
+    result.innerHTML = `
+    <div class="info">
+      <img src=${movie.Poster} class="poster">
+        <div>
+          <h2 class="movieTitle">${movie.Title}</h2>
+          <h4 class="imdb">IMDb: ${movie.imdbRating}</h4>
+          <div class="ratingRuntime">
+            <span class="raiting">${movie.Rated}</span>
+            <span class="year">${movie.Year}</span>
+            <span class="runtime">${movie.Runtime}</span>
+          </div>
+        </div>
+    </div>
+  `;
+  }
+}
